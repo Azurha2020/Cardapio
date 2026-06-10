3fdb2d468ff0c3f7537b338f9e70a9eb0e1205ec
Azurha2020 <jurityroberto@gmail.com>
2026-06-10 14:24:24 -0300

    Update SQLCardapio.sql

--- Cardapio/src/main/java/recurso/SQLCardapio.sql
+++ Cardapio/src/main/java/recurso/SQLCardapio.sql
@@ -12,16 +12,29 @@ create table if not exists Ingredientes(
     grupo int,
     constraint fkGrupo foreign key (grupo) references Grupos(id)
 );
-create table Pratos(
+create TABLE IF NOT EXISTS Pratos(
 	id int primary key,
     nome varchar(100),
     preparo text,
     porcoes int,
     tempo int
 );
+CREATE TABLE IF NOT EXISTS Refeicoes(
+id int PRIMARY KEY auto increment,
+tempo_max int,
+tempo_ttl int,
+caloria_Max int,
+caloria_ttl int
+);
 create table if not exists Ingrediente_Prato(
 	ingrediente int,
     prato int,
     constraint fk_Ing foreign key (ingrediente) references Ingredientes(id),
     constraint fk_prato foreign key (prato) references Pratos(id)
+);
+CREATE TABLE IF NOT EXISTS Pratos_refeicao(
+	prato int,
+	refeicao int,
+	CONSTRAINT fk_prato FOREIGN KEY (prato) REFERENCES Pratos(id),
+	CONSTRAINT fk_refeicao FOREIGN KEY (refeicao) REFERENCES Refeicoes(id)
 );
